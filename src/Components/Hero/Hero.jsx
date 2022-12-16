8e5d41f7d6db1a666342c010eeba33e62f82b7c3
adeolammy <adeolammy2020@gmail.com>
2022-12-16 14:23:31 +0100

    Including some images and adding some more components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { Button, Container, MainHeading } from '../../globalStyle';
-import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './HeroStyleHero'
+import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './HeroStyle';
 
 const Hero = () => {
   return (
     <div>
       <HeroSection>
-          <HeroVideo src="./assets/hero.mp4"/>
+          <HeroVideo src="./assests/hero.mp4"/>
           <Container>
               <MainHeading>Your data is secure with us</MainHeading>
               <HeroText>We provide the best security systems for clients all over the world</HeroText>
